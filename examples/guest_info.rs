07b2624171a4d816f8a672b16006d6c575310df3
arcnmx <arcnmx@users.noreply.github.com>
2018-03-07 01:24:39 -0500

    fix examples compiling with any feature combo

--- examples/guest_info.rs
+++ examples/guest_info.rs
@@ -1,19 +1,29 @@
-extern crate env_logger;
 extern crate qapi;
+extern crate env_logger;
 
-use std::os::unix::net::UnixStream;
-use std::env::args;
-use qapi::{qga, Qga};
+#[cfg(feature = "qga")]
+mod main {
+    use std::os::unix::net::UnixStream;
+    use std::env::args;
+    use qapi::{qga, Qga};
 
-fn main() {
-    env_logger::init();
+    pub fn main() {
+        ::env_logger::init();
 
-    let socket_addr = args().nth(1).expect("argument: QEMU Guest Agent socket path");
-    let stream = UnixStream::connect(socket_addr).expect("failed to connect to socket");
+        let socket_addr = args().nth(1).expect("argument: QEMU Guest Agent socket path");
+        let stream = UnixStream::connect(socket_addr).expect("failed to connect to socket");
 
-    let mut qga = Qga::from_stream(&stream);
+        let mut qga = Qga::from_stream(&stream);
 
-    qga.handshake().expect("handshake failed");
-    let info = qga.execute(&qga::guest_info { }).unwrap().unwrap();
-    println!("Guest Agent version: {}", info.version);
+        qga.handshake().expect("handshake failed");
+        let info = qga.execute(&qga::guest_info { }).unwrap().unwrap();
+        println!("Guest Agent version: {}", info.version);
+    }
 }
+
+#[cfg(not(feature = "qga"))]
+mod main {
+    pub fn main() { panic!("requires feature qga") }
+}
+
+fn main() { main::main() }
--- examples/qmp_query.rs
+++ examples/qmp_query.rs
@@ -1,32 +1,42 @@
 extern crate env_logger;
 extern crate qapi;
 
-use std::thread::sleep;
-use std::time::Duration;
-use std::env::args;
-use std::os::unix::net::UnixStream;
-use qapi::{qmp, Qmp};
+#[cfg(feature = "qmp")]
+mod main {
+    use std::thread::sleep;
+    use std::time::Duration;
+    use std::env::args;
+    use std::os::unix::net::UnixStream;
+    use qapi::{qmp, Qmp};
 
-fn main() {
-    env_logger::init();
+    pub fn main() {
+        ::env_logger::init();
 
-    let socket_addr = args().nth(1).expect("argument: QMP socket path");
-    let stream = UnixStream::connect(socket_addr).expect("failed to connect to socket");
+        let socket_addr = args().nth(1).expect("argument: QMP socket path");
+        let stream = UnixStream::connect(socket_addr).expect("failed to connect to socket");
 
-    let mut qmp = Qmp::from_stream(&stream);
+        let mut qmp = Qmp::from_stream(&stream);
 
-    let info = qmp.handshake().expect("handshake failed");
-    println!("QMP info: {:#?}", info);
+        let info = qmp.handshake().expect("handshake failed");
+        println!("QMP info: {:#?}", info);
 
-    let status = qmp.execute(&qmp::query_status { }).unwrap().unwrap();
-    println!("VCPU status: {:#?}", status);
+        let status = qmp.execute(&qmp::query_status { }).unwrap().unwrap();
+        println!("VCPU status: {:#?}", status);
 
-    loop {
-        qmp.nop().unwrap();
-        for event in qmp.events() {
-            println!("Got event: {:#?}", event);
+        loop {
+            qmp.nop().unwrap();
+            for event in qmp.events() {
+                println!("Got event: {:#?}", event);
+            }
+
+            sleep(Duration::from_secs(1));
         }
-
-        sleep(Duration::from_secs(1));
     }
 }
+
+#[cfg(not(feature = "qmp"))]
+mod main {
+    pub fn main() { panic!("requires feature qmp") }
+}
+
+fn main() { main::main() }
