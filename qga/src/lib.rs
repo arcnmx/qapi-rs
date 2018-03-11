091766b62372aacf81b83edd7c8450d7250c2e8b
arcnmx <arcnmx@users.noreply.github.com>
2018-03-11 00:06:55 -0500

    GuestExecStatus helpers

--- qga/src/lib.rs
+++ qga/src/lib.rs
@@ -6,3 +6,45 @@ extern crate serde;
 extern crate serde_derive;
 
 include!(concat!(env!("OUT_DIR"), "/qga.rs"));
+
+use std::{io, str};
+
+impl GuestExecStatus {
+    pub fn result(self) -> Result<Self, Self> {
+        if self.exited {
+            if self.exitcode != Some(0) || self.signal.is_some() {
+                Err(self)
+            } else {
+                Ok(self)
+            }
+        } else {
+            Ok(self)
+        }
+    }
+}
+
+impl From<GuestExecStatus> for io::Error {
+    fn from(s: GuestExecStatus) -> Self {
+        let (err0, err1) = if let Some(Ok(data)) = s.err_data.as_ref().map(|s| str::from_utf8(s)) {
+            (": ", data)
+        } else {
+            ("", "")
+        };
+
+        let sig = if let Some(signal) = s.signal {
+            format!(" (terminated by signal {})", signal)
+        } else {
+            Default::default()
+        };
+
+        let msg = if let Some(code) = s.exitcode {
+            format!("process exited with code {}{}{}{}", code, sig, err0, err1)
+        } else if s.exited {
+            format!("process exited{}{}{}", sig, err0, err1)
+        } else {
+            panic!("a running process is not an error")
+        };
+
+        io::Error::new(io::ErrorKind::Other, msg)
+    }
+}
