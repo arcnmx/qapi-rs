0a6d8c299e5e86afb7d0f509bf06a58040b43d61
arcnmx <arcnmx@users.noreply.github.com>
2018-03-13 18:02:46 -0400

    device_add constructor

--- qmp/src/lib.rs
+++ qmp/src/lib.rs
@@ -5,6 +5,8 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+use std::string;
+
 include!(concat!(env!("OUT_DIR"), "/qmp.rs"));
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -16,4 +18,20 @@ pub struct QMP {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct QapiCapabilities {
     pub QMP: QMP,
+}
+
+impl device_add {
+    pub fn new(driver: string::String, id: Option<string::String>, bus: Option<string::String>, props: qapi::Dictionary) -> Self {
+        let mut dict = qapi::Dictionary::default();
+        dict.insert("driver".into(), qapi::Any::String(driver));
+        if let Some(id) = id {
+            dict.insert("id".into(), qapi::Any::String(id));
+        }
+        if let Some(bus) = bus {
+            dict.insert("bus".into(), qapi::Any::String(bus));
+        }
+        dict.extend(props.into_iter());
+
+        device_add(dict)
+    }
 }
