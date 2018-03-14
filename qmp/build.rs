d4a02e24385d9f08b5a2e9eda0193cdf30fc1065
arcnmx <arcnmx@users.noreply.github.com>
2018-03-13 20:42:02 -0400

    fix packaging of qapi schema

--- qmp/build.rs
+++ qmp/build.rs
@@ -13,7 +13,7 @@ fn main_result() -> io::Result<()> {
     println!("rerun-if-changed=build.rs");
 
     let out_dir = path::Path::new(&env::var("OUT_DIR").unwrap()).join("qmp.rs");
-    let schema_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/../schema/");
+    let schema_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/schema/");
 
     for inc in qapi_codegen::codegen(schema_dir, out_dir)? {
         println!("rerun-if-changed={}", inc.display());
