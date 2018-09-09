f43fb373198a4103585973121ca9faa1598643cc
ghost <ghost@konpa.ku>
2018-09-09 23:56:24 +0000

    0.3.0 - c1c7ee9f9e7b990ea375c46eacb3661220a054d7

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["qapi_parser"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"qapi_parser/spec/struct.Type.html\" title=\"struct qapi_parser::spec::Type\">Type</a>",synthetic:false,types:["qapi_parser::spec::Type"]},];
 implementors["qapi_spec"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"qapi_spec/enum.ErrorClass.html\" title=\"enum qapi_spec::ErrorClass\">ErrorClass</a>",synthetic:false,types:["qapi_spec::ErrorClass"]},];
 
             if (window.register_implementors) {
