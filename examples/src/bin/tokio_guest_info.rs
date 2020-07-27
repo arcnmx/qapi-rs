9630b058dc30b8a657d0ec818443751631b1d270
arcnmx <arcnmx@users.noreply.github.com>
2020-07-26 19:58:46 -0700

    clean up examples

--- examples/src/bin/tokio_guest_info.rs
+++ examples/src/bin/tokio_guest_info.rs
@@ -8,17 +8,17 @@ async fn main() -> io::Result<()> {
     let socket_addr = args().nth(1).expect("argument: QEMU Guest Agent socket path");
 
     let stream = qapi::futures::QgaStreamTokio::open_uds(socket_addr).await?;
-    let (stream, handle) = stream.spawn_tokio();
+    let (qga, handle) = stream.spawn_tokio();
 
-    let sync_value = &stream as *const _ as usize as isize;
-    stream.guest_sync(sync_value).await?;
+    let sync_value = &qga as *const _ as usize as isize;
+    qga.guest_sync(sync_value).await?;
 
-    let info = stream.execute(qapi::qga::guest_info { }).await?;
+    let info = qga.execute(qapi::qga::guest_info { }).await?;
     println!("Guest Agent version: {}", info.version);
 
     {
         // NOTE: this isn't necessary, but to manually ensure the stream closes...
-        drop(stream); // relinquish handle on the stream
+        drop(qga); // relinquish handle on the stream
         handle.await?; // wait for event loop to exit
     }
 
--- examples/src/bin/tokio_qmp_query.rs
+++ examples/src/bin/tokio_qmp_query.rs
@@ -10,14 +10,14 @@ pub async fn main() -> io::Result<()> {
     let stream = qapi::futures::QmpStreamTokio::open_uds(socket_addr).await?;
     println!("{:#?}", stream.capabilities);
     let stream = stream.negotiate().await?;
-    let (stream, handle) = stream.spawn_tokio();
+    let (qmp, handle) = stream.spawn_tokio();
 
-    let status = stream.execute(qapi::qmp::query_status { }).await?;
+    let status = qmp.execute(qapi::qmp::query_status { }).await?;
     println!("VCPU status: {:#?}", status);
 
     {
         // NOTE: this isn't necessary, but to manually ensure the stream closes...
-        drop(stream); // relinquish handle on the stream
+        drop(qmp); // relinquish handle on the stream
         handle.await?; // wait for event loop to exit
     }
 
