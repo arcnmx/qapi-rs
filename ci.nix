2dfa56c07d13c035d2123e36b269787c131e1ed6
arcnmx <git@git.arcn.mx>
2023-01-27 10:53:55 -0800

    chore(ci): migrate ci.nix

--- ci.nix
+++ ci.nix
@@ -1,23 +1,5 @@
-{ config, channels, pkgs, lib, ... }: with pkgs; with lib; let
-  inherit (import ./. { pkgs = null; }) checks packages devShells;
-  importShell = pkgs.writeText "shell.nix" ''
-    import ${builtins.unsafeDiscardStringContext devShells.default.drvPath}
-  '';
-  cargo = name: command: pkgs.ci.command {
-    name = "cargo-${name}";
-    command = ''
-      nix-shell ${importShell} --run ${escapeShellArg ("cargo " + command)}
-    '';
-    impure = true;
-  };
-  commas = concatStringsSep ",";
-  featureMatrix = rec {
-    qmp = singleton "qmp";
-    qga = singleton "qga";
-    all = qmp ++ qga;
-    async = all ++ [ "async-tower" ];
-    tokio = all ++ singleton "async-tokio-all";
-  };
+{ config, pkgs, lib, ... }: with pkgs; with lib; let
+  inherit (import ./. { pkgs = null; }) checks packages;
 in {
   config = {
     name = "qapi-rs";
@@ -27,18 +9,17 @@ in {
       nixpkgs = "22.11";
     };
     tasks = with checks; {
-      test.inputs = mapAttrsToList (key: features:
-        cargo "test-${key}" "test -p qapi --features ${commas features}"
-      ) featureMatrix;
-      build.inputs = mapAttrsToList (key: features:
-        cargo "build-${key}" "build -p qapi --features ${commas features}"
-      ) featureMatrix;
-      parser.inputs = singleton (cargo "qapi-parser" "test -p qapi-parser");
-      spec.inputs = singleton (cargo "qapi-spec" "test -p qapi-spec");
-      codegen.inputs = singleton (cargo "qapi-codegen" "test -p qapi-codegen");
-      qga.inputs = singleton (cargo "qapi-qga" "test -p qapi-qga");
-      qmp.inputs = singleton (cargo "qapi-qmp" "test -p qapi-qmp");
-      examples.inputs = singleton (cargo "examples" "build --examples --bins");
+      test.inputs = [
+        test-qapi test-qapi-all
+        test-qapi-qmp test-qapi-qga
+        test-qapi-async test-qapi-tokio
+      ];
+      parser.inputs = [ checks.test-parser ];
+      spec.inputs = [ checks.test-spec ];
+      codegen.inputs = [ checks.test-codegen ];
+      qga.inputs = [ checks.test-qga ];
+      qmp.inputs = [ checks.test-qmp ];
+      examples.inputs = [ packages.examples ];
     };
     jobs = {
       nixos = {
@@ -46,7 +27,7 @@ in {
           windows.inputs = [ packages.examples-windows ];
         };
       };
-      # XXX: macos.system = "x86_64-darwin";
+      macos.system = "x86_64-darwin";
     };
   };
 }
