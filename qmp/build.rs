ea4d4cde2dd21405b347fd4dedeffe3fbf6df01f
David Tolnay <dtolnay@gmail.com>
2023-05-03 01:17:42 -0700

    Respect manifest dir set during build script execution

--- qmp/build.rs
+++ qmp/build.rs
@@ -13,7 +13,9 @@ fn main_result() -> io::Result<()> {
     println!("rerun-if-changed=build.rs");
 
     let out_dir = path::Path::new(&env::var("OUT_DIR").unwrap()).join("qmp.rs");
-    let schema_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/schema/qapi/");
+    let schema_dir = path::Path::new(&env::var_os("CARGO_MANIFEST_DIR").unwrap())
+        .join("schema")
+        .join("qapi");
 
     for inc in qapi_codegen::codegen(schema_dir, out_dir, "QmpCommand".into())? {
         println!("rerun-if-changed={}", inc.display());
